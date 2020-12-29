b6094634f265e86cf4b4584d119c5cefe7f84bad
horiga <h.horiga@gmail.com>
2020-12-29 15:14:45 +0900

    Apply micrometer prometheus

--- README.md
+++ README.md
@@ -27,3 +27,37 @@ $ sh fire-and-forget.sh
 $ sh request-stream.sh
 ```
 
+## Monitoring
+
+Micrometer Prometheus
+
+```
+http://localhost:18080/actuator/prometheus
+```
+
+Metrics
+
+```
+rsocket_request_response_seconds_count{signal_type="CANCEL",} 0.0
+rsocket_request_response_seconds_sum{signal_type="CANCEL",} 0.0
+rsocket_request_response_seconds_count{signal_type="ON_ERROR",} 0.0
+rsocket_request_response_seconds_sum{signal_type="ON_ERROR",} 0.0
+rsocket_request_response_seconds_count{signal_type="ON_COMPLETE",} 2.0
+rsocket_request_response_seconds_sum{signal_type="ON_COMPLETE",} 0.003192502
+rsocket_request_response_seconds_max{signal_type="CANCEL",} 0.0
+rsocket_request_response_seconds_max{signal_type="ON_ERROR",} 0.0
+rsocket_request_response_seconds_max{signal_type="ON_COMPLETE",} 0.001779824
+rsocket_request_channel_total{signal_type="CANCEL",} 0.0
+rsocket_request_channel_total{signal_type="ON_ERROR",} 0.0
+rsocket_request_channel_total{signal_type="ON_COMPLETE",} 0.0
+rsocket_metadata_push_total{signal_type="CANCEL",} 0.0
+rsocket_metadata_push_total{signal_type="ON_ERROR",} 0.0
+rsocket_metadata_push_total{signal_type="ON_COMPLETE",} 0.0
+rsocket_request_stream_total{signal_type="CANCEL",} 0.0
+rsocket_request_stream_total{signal_type="ON_ERROR",} 0.0
+rsocket_request_stream_total{signal_type="ON_COMPLETE",} 1.0
+rsocket_request_fnf_total{signal_type="CANCEL",} 0.0
+rsocket_request_fnf_total{signal_type="ON_ERROR",} 0.0
+rsocket_request_fnf_total{signal_type="ON_COMPLETE",} 0.0
+```
+
--- src/main/kotlin/org/horiga/study/rsocket/BootDockerApplication.kt
+++ src/main/kotlin/org/horiga/study/rsocket/BootDockerApplication.kt
@@ -1,8 +1,18 @@
 package org.horiga.study.rsocket
 
+import io.micrometer.core.instrument.MeterRegistry
+import io.rsocket.core.Resume
+import io.rsocket.micrometer.MicrometerDuplexConnectionInterceptor
+import io.rsocket.micrometer.MicrometerRSocketInterceptor
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.rsocket.server.RSocketServerCustomizer
+import org.springframework.boot.rsocket.server.ServerRSocketFactoryProcessor
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import reactor.util.retry.Retry
+import java.time.Duration
 
 annotation class NoArgs
 
@@ -14,4 +24,27 @@ fun main(args: Array<String>) {
 }
 
 @Configuration
-class RsocketServerConfiguration
+class RsocketServerConfiguration {
+
+    companion object {
+        val log = LoggerFactory.getLogger(RsocketServerConfiguration::class.java)!!
+    }
+
+    // TODO: `ServerRSocketFactoryProcessor` is deprecated. should change to use `RSocketServerCustomizer`.
+    @Bean
+    fun serverRSocketFactoryProcessor(meterRegistry: MeterRegistry) = ServerRSocketFactoryProcessor { factory ->
+        factory.addResponderPlugin(MicrometerRSocketInterceptor(meterRegistry))
+    }
+//
+//    @Bean
+//    fun rSocketServerCustomizer(): RSocketServerCustomizer = RSocketServerCustomizer { server ->
+//        server.resume(Resume()
+//                          .sessionDuration(Duration.ofMinutes(5))
+//                          .retry(
+//                              Retry.fixedDelay(Long.MAX_VALUE, Duration.ofSeconds(5)).doBeforeRetry {
+//                                  log.info("Disconnected. trying to resume...")
+//                              }
+//                          )
+//        )
+//    }
+}
